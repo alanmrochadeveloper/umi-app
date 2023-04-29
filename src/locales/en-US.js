ef80f0c8762db56630c62b7dc8ccc0b82a288049
Alan Miguel Rocha <alanmrochadeveloper@gmail.com>
2023-04-29 08:57:02 -0300

    feat: add internacionalization between pt-BR en-US

---
+++ src/locales/en-US.js
@@ -0,0 +1,8 @@
+import menu from './en-US/menu';
+
+export default {
+  ...menu,
+  'home.recents': ' Recent opportunities',
+  'greetings.hello': 'Hello',
+  'greetings.welcome': 'welcome',
+}
---
+++ src/locales/pt-BR.js
@@ -0,0 +1,7 @@
+import menu from './pt-BR/menu'
+export default {
+  ...menu,
+  'home.recents': 'Oportunidades Recentes',
+  'greetings.hello': 'Olá',
+  'greetings.welcome': 'Bem vindo',
+}
